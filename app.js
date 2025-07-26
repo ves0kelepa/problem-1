9bd3b79f0ce24bd1782a2c3ecb01cdca45eb15f6
ves0kelepa <148004302+ves0kelepa@users.noreply.github.com>
2025-07-26 22:50:14 +0300

    Add TODO for credential validation

--- app.js
+++ app.js
@@ -2,3 +2,5 @@ console.log('Hello from master')
 console.log('Change app.js in master')
 function login() {console.log('Login logic ghere');}
 console.log('Add basic login infromation')
+// TODO: validate credentials
+
